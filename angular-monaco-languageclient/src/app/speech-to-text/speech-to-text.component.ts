4ac26ead1806b4213041129e9ca817d4bdabb61e
Drigesh <drigesh.anuragi@codingninjas.com>
2023-02-11 15:27:45 +0530

    add keypress events

--- angular-monaco-languageclient/src/app/speech-to-text/speech-to-text.component.ts
+++ angular-monaco-languageclient/src/app/speech-to-text/speech-to-text.component.ts
@@ -224,6 +224,10 @@ export class SpeechToTextComponent implements AfterViewInit {
     //   .subscribe(res => console.log('LONG CLICK'));
   }
 
+  onKeyPress(){
+    
+  }
+
   ngAfterViewInit() {
     fromEvent(this.el.nativeElement, 'mousedown').subscribe(() => {
       this.listening = true;
@@ -233,6 +237,14 @@ export class SpeechToTextComponent implements AfterViewInit {
       this.listening = false;
       this.stopSpeechRecognition();
     });
+    fromEvent(this.el.nativeElement, 'keydown').subscribe(() => {
+      this.listening = true;
+      this.startSpeechRecognition();
+    })
+    fromEvent(this.el.nativeElement, 'keyup').subscribe(() => {
+      this.listening = false;
+      this.stopSpeechRecognition();
+    })
   }
 }
 
